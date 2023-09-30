ff66dd01fc6181da51236c70da28e513adb421fe
yzy <yuzhiyuancs@gmail.com>
2023-09-30 23:39:27 +0800

    2022 9.30 23:39

--- DP/p647_countSubstrings.go
+++ DP/p647_countSubstrings.go
@@ -7,14 +7,15 @@ import "fmt"
 
 func Problem647() {
 	s := "abc"
-	fmt.Println(countSubstrings(s))
+	fmt.Println(countSubstrings1(s))
+	fmt.Println(countSubstrings2(s))
 }
 
 // 方法一：动态规划:dp[i][j]：表示区间范围[i,j]的子串是否是回文子串
 // 初始化：dp[i][j] = flase
 // dp[i][j]=true 的情况 大前提*** s[i] == s[j]：情况1：j-i<=1 情况2：dp[i+1][j-1] = true
 // 遍历顺序：dp[i][j]取决于dp[i+1][j-1] 取决于左下角的值 那么一定是从左下角往右上角遍历 （i: n-1~0 j:i~n-1 j>=i)
-func countSubstrings(s string) int {
+func countSubstrings1(s string) int {
 	n := len(s)
 	result := 0
 	// 初始化值为false
@@ -36,4 +37,25 @@ func countSubstrings(s string) int {
 		}
 	}
 	return result
+}
+
+// 方法二：中心扩展：以中心点往来两边扩展 如果两边字符相等 回文子串++
+// 中心2n-1个：单字符中心n , 双字符中心n-1
+func countSubstrings2(s string) int {
+	n := len(s)
+	ans := n // 一个字符的均为回文串
+	// 单中心扩展
+	for i := 0; i <= n; i++ {
+		left, right := i-1, i+1
+		for left >= 0 && right < n {
+			if s[left] == s[right] {
+				ans++
+				left--
+				right++
+			} else {
+				break
+			}
+		}
+	}
+	//双中心结点扩展
 }
